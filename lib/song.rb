fff0ac4419cc95fd8f5ff4566f01a27c423fa5c8
Rabah <rmerzoug471@gmail.com>
2022-08-08 23:50:37 -0500

    commit tree 79b701a94d2c835e9dc957f82ac2bd499de7957e

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@genre = []
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count +=1
+        @@genre << genre
+    end  
+    def self.count 
+        @@count
+    end
+    def self.genre
+        @@genre.uniq
+    end
+end
+binding.pry
